071e852ec057d48e08dffa5b2e4329ad32c84ead
hakansuluoglu <hakansuluoglu@gmail.com>
2019-03-16 15:39:23 +0300

    init splash

---
+++ app/src/main/java/com/hakansu/restgraphql/presentation/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.hakansu.restgraphql.presentation.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hakansu.restgraphql.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
