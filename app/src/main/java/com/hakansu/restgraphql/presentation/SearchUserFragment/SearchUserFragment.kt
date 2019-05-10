4e4ae0cf994860cd699af2db74b8d882c36f17b7
hakansuluoglu <hakansuluoglu@gmail.com>
2019-05-10 15:05:12 +0300

    wtf splash

--- app/src/main/java/com/hakansu/restgraphql/presentation/SearchUserFragment/SearchUserFragment.kt
+++ app/src/main/java/com/hakansu/restgraphql/presentation/SearchUserFragment/SearchUserFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import butterknife.ButterKnife
 import com.hakansu.restgraphql.R
 import com.hakansu.restgraphql.App
 import javax.inject.Inject
@@ -28,7 +27,6 @@ class SearchUserFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_rest, container, false)
-        ButterKnife.bind(this,rootView)
         naber()
         return rootView
     }
--- app/src/main/java/com/hakansu/restgraphql/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/hakansu/restgraphql/presentation/splash/SplashActivity.kt
@@ -2,24 +2,78 @@ package com.hakansu.restgraphql.presentation.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import butterknife.ButterKnife
-import butterknife.OnClick
-import com.hakansu.restgraphql.R
 import com.hakansu.restgraphql.presentation.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import com.hakansu.restgraphql.R
+
 
 class SplashActivity : AppCompatActivity() {
+
+    var clickCount = 0
+    var timer:TimerTask ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        ButterKnife.bind(this)
+
+        val initialText = "Merhaba arkadaşlar uygulamama hoşgeldiniz.. " +
+                "\nBirazdan aşağıda bir buton belierecek. " +
+                "\nBöyle bir ekran olmazsa olmaz biliyorsunuz " +
+                "\nneyse tıkla hadi..."
+        textView.text = ""
+        addtext(initialText)
+        button.setOnClickListener { onClick() }
     }
 
-    @OnClick(R.id.button)
-    fun onClick(){
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    fun addtext(naber:String) {
+        val size = naber.length
+        var count = 0
+
+        timer =  Timer().scheduleAtFixedRate(0, 40) {
+            runOnUiThread {
+                if (count < size) {
+                    textView?.text = textView?.text.toString().plus(naber[count])
+                    count++
+                } else {
+                    button.animate()
+                        .translationY(0f)
+                        .alpha(1.0f)
+                        .setDuration(10000)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationStart(animation: Animator?) {
+                                super.onAnimationStart(animation)
+                                button.visibility = View.VISIBLE
+                                timer?.cancel()
+                                timer = null
+                            }
+                        })
+                }
+            }
+        }
     }
 
+    fun onClick() {
+        clickCount++
+        if (clickCount == 1) {
+            button.visibility = View.GONE
+            addtext(
+                "\n\n......" +
+                        "\nBu tıklamayı beğenmedim........... " +
+                        "\nBen bas dediğimde arka arkaya 4 kere tıkla." +
+                        "\nhızlı ol..." +
+                        "\nHADİ BAŞLA!"
+            )
+        }
+        if (clickCount == 5) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
 }
+
